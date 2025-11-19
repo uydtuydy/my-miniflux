279ee46c00f3c4676cbaba7840caf8dbe6064f2b
uydtuydy <uydt2000@163.com>
2025-11-19 11:58:50 +0800

    Create start.sh for Miniflux setup and execution
    Add script to download and run Miniflux with memory database

--- start.sh
+++ start.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# 下载最新版 Miniflux
+wget -O miniflux https://github.com/miniflux/v2/releases/latest/download/miniflux-linux-amd64
+chmod +x miniflux
+
+# 使用内存数据库（简化）
+export DATABASE_URL=:memory:
+export RUN_MIGRATIONS=1
+export CREATE_ADMIN=1
+export ADMIN_USERNAME=admin
+export ADMIN_PASSWORD=123456
+export PORT=$PORT
+
+# 启动
+./miniflux
